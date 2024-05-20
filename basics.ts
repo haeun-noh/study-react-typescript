76b365aaedf9af028534d43c2434c881af558bbb
haeunNoh06 <okhagrace06@naver.com>
2024-05-20 14:01:51 +0900

    :sparkles: [feat] typescript 복잡한 자료형
    - arrays, object, null

--- basics.ts
+++ basics.ts
@@ -15,5 +15,30 @@ let isInstructor: boolean;
 isInstructor = true;
 
 
-let hobbies: null;
+// let hobbies: null;
 // hobbies = 12;// 오류 발생
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+var person: {
+	name: string;
+	age: number;
+}
+person = {
+	name: 'Max',
+	age: 32
+};
+
+// var person: any;
+
+// person = {
+// 	// isEmployee: true;// 오류 발생
+// };
+
+// 배열로 만들려면 뒤에 []를 붙여야 함.
+let people: {
+	name: string;
+	age: number;
+}[];
