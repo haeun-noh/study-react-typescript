f9336ca42e29f2d8e23adb814ddb474e2c0e1042
haeunNoh06 <okhagrace06@naver.com>
2024-05-20 13:47:40 +0900

    :sparkles: [feat] typescript 기본형

---
+++ basics.ts
@@ -0,0 +1,19 @@
+/**
+Primitives: number, string, boolean
+More complex types: arrays, objects
+Funtion types, parameters
+ */
+
+let age: number; // 변수 선언
+
+age = 12;
+age = 12.1; // 실수도 포함
+
+let userName: string; // 소문자로 시작한다.
+
+let isInstructor: boolean;
+isInstructor = true;
+
+
+let hobbies: null;
+// hobbies = 12;// 오류 발생
