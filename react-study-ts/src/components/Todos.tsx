eaac63cfcce47cb746813cd6de7a868e22e55561
haeunNoh06 <okhagrace06@naver.com>
2024-05-30 13:33:16 +0900

    :memo: [study] react 시작하기

---
+++ react-study-ts/src/components/Todos.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// React.FC: 리액트 패키지에 정의된 타입
+// 이를 통해 이 함수가 함수형 컴포넌트로 동작한다는 걸 명확히 한다. 
+const Todos: React.FC = (props) => {
+    return <ul>
+        
+    </ul>
+}
+
+export default Todos;// export를 통해 파일 밖에서도 접근 가능하도록 설정
