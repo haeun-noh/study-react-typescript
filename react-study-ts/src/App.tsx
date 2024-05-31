8e3cc7c7cf836d7be461bde82d7a4e24a9181287
haeunNoh06 <okhagrace06@naver.com>
2024-05-31 09:54:19 +0900

    :sparkles: [feat] state 및 타입스크립트 관리하기
    - setTodos로 새 할 일 추가하기

--- react-study-ts/src/App.tsx
+++ react-study-ts/src/App.tsx
@@ -1,18 +1,25 @@
+import {useState} from 'react';
+
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
   // state를 사용해 이 배열의 상태를 관리해야 함. 
-  const todos = [// 문자열 배열이 아닌 Todo 객체 배열
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ];
-  // 코드에 이상이 없고 구조가 명확함을 알 수 있다. 
+  // 문제발생 - 빈 배열을 넘김으로써 어떤 타입의 배열이 나올지를 예상하지 못함.
+  // todos의 타입이 never인 이유임.
+  // 이건 이 배열이 항상 비어있어야 한다는 뜻
+  // useState는 제네릭 함수이므로 어떤 타입의 배열인지를 알려줄 수 있다. 
+  const [todos, setTodos] = useState<Todo[]>([]);// 이 state가 Todo 배열을 관리한다. 
 
   // onAddTodo의 파라미터가 될 함수
   const addTodoHandler = (todoText: string) => {
     // TODO: state를 사용해 배열 관리하기 
+
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
   }
 
   return (
